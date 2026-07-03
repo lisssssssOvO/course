855af39ac94d343b59a810433c2e0e08f25a4511
lisssssssOvO <1906fox2003@gmail.com>
2026-07-03 23:33:11 +0300

    Добавлено задание 1 Типы данных

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace CompoundInterestCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = CalculateCompoundInterest(1000, 3, 10);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        public static string CalculateCompoundInterest(double initialDeposit, int years, double interestRate)
+        {
+            var result = new StringBuilder();
+            double currentAmount = initialDeposit;
+
+            for (int year = 1; year <= years; year++)
+            {
+                currentAmount *= (1 + interestRate / 100);
+                result.AppendLine($"Год {year}: {currentAmount:F2} руб.");
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
