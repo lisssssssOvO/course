512275ce8f0967c10c3e70c4cacc655284078510
lisssssssOvO <1906fox2003@gmail.com>
2026-07-08 15:50:27 +0300

    Добавлено задание 2 Типы данных

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static void PrintDiamond(int N)
+    {
+        if (N <= 0 || N % 2 == 0)
+        {
+            Console.WriteLine("N должно быть положительным нечётным числом");
+            return;
+        }
+
+        int center = N / 2;
+
+        for (int i = 0; i < N; i++)
+        {
+            int spaces = Math.Abs(center - i);
+            int middle = N - 2 - spaces * 2;
+
+            Console.Write(new string(' ', spaces) + "X");
+
+            if (middle > 0)
+            {
+                Console.Write(new string(' ', middle) + "X");
+            }
+
+            Console.WriteLine();
+        }
+    }
+
+    public static void Main()
+    {
+        PrintDiamond(5);
+    }
+}
